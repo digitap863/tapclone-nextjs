1a98806bd23b8d45e163537d7ff8deed453572bd
digitap863 <new.tapclone@gmail.com>
2025-07-30 12:58:02 +0530

    tag manager pos chaneged

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,21 +17,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-          {/* Google Tag Manager Script */}
-         <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K4S3MRWC');
-            `,
-          }}
-        />
-
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -71,9 +56,31 @@ export default function RootLayout({ children }) {
           '--font-monument-ultrabold': '"Monument Extended Ultrabold", sans-serif',
         }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4S3MRWC"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        
         {/* Google Tag Manager Script */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K4S3MRWC"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K4S3MRWC');
+            `,
+          }}
+        />
+        
         {children}
         <div id="star-root"></div>
       </body>
